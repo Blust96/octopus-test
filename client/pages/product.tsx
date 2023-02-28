8be4dd126b596f8760322a2d5cd875e2ea26e400
Blust96 <q.bohain@gmail.com>
2023-02-28 23:38:29 +0100

    feat(pages/product): product fetch

--- client/pages/product.tsx
+++ client/pages/product.tsx
@@ -1,3 +1,45 @@
-export default function Product() {
-  return <div>Product page</div>
+import { useQuery } from '@tanstack/react-query'
+import request from 'graphql-request'
+import type { InferGetServerSidePropsType } from 'next'
+
+import { API_ENDPOINT } from '@/config/api'
+import { graphql } from '@/gql'
+import type { QueryProductByIdQuery } from '@/gql/graphql'
+
+const queryProductById = graphql(`
+  query queryProductById($id: ID!) {
+    Product(id: $id) {
+      id
+      name
+      power
+      description
+      price
+      quantity
+      brand
+      weight
+      height
+      width
+      length
+      model_code
+      colour
+      img_url
+    }
+  }
+`)
+
+const getProductById = async (id: string) => request(API_ENDPOINT, queryProductById, { id })
+
+export default function Product({
+  product,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const { data } = useQuery<QueryProductByIdQuery>(['product'], async () => getProductById('1'), {
+    initialData: product,
+  })
+
+  return <div>{data?.Product?.name}</div>
+}
+
+export const getServerSideProps = async () => {
+  const product = await getProductById('1')
+  return { props: { product } }
 }
